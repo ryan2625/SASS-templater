bc24c61cfe4fc9230087f4874b82bf4cf0aa25a4
ryan2625 <ryanfreas19@augustana.edu>
2024-10-12 13:05:39 -0500

    Updating slider classes

--- src/Helpers/sliders.ts
+++ src/Helpers/sliders.ts
@@ -1,18 +1,24 @@
 class SliderContainer {
-    constructor() {
+    sliderList: Slider[]
 
+    constructor(sliderList: Slider[]) {
+        this.sliderList = sliderList
     }
 
     create():Slider {
-        return new Slider(1, 1, null)
+        return new Slider(1, 1, null, null)
+    }
+
+    edit() {
+        
     }
 }
 
 class Slider {
     position: number
     value: number 
-    prev: Slider
-    next: Slider
+    prev: Slider | null
+    next: Slider | null
 
     constructor(position: number, value: number, prev: Slider | null, next: Slider | null ) {
         this.position = position
