58abed9efc3a132fbf8c58128bc42a5eaed19ff5
ryan2625 <ryanfreas19@augustana.edu>
2024-10-13 15:18:31 -0500

    Updating slider class handling adding to slider arr

--- src/Helpers/sliders.ts
+++ src/Helpers/sliders.ts
@@ -1,16 +1,33 @@
+interface sliderInstanceProps {
+    prev: null,
+    id: string
+}
+
 class SliderContainer {
     public sliderList: Slider[]
+    public len: number
     private body: HTMLDivElement
     private styles: string
+
 
     constructor(sliderList: Slider[]) {
         this.sliderList = []
         this.body = document.createElement("div")
         this.styles = ``
+        this.len = 0
+        //Append it to parent div element 
     }
 
-    addSlider(slider:Slider) {
-        this.sliderList.push(slider)
+    public createAndAddSlider(slider: sliderInstanceProps) {
+        if (this.len > 0) {
+            const prevSlider:Slider = this.sliderList[this.len - 1]
+            var newSlider = new Slider(prevSlider, slider.id)
+            prevSlider.next = newSlider
+        } else {
+            var newSlider = new Slider(null, slider.id)
+        }
+        this.len += 1
+        this.sliderList.push(newSlider)
     }
 }
 
@@ -19,23 +36,35 @@ class SliderContainer {
 //Update value function to ensure the position is not greater than the next position. Add event listeners.
 
 class Slider {
+    public prev: Slider | null
+    public next: Slider | null
+    public id: string
     private position: number
-    private value: number 
-    private prev: Slider | null
-    private next: Slider | null
+    private value: number
     private body: HTMLDivElement
     private styles: string
 
-    constructor(value: number, prev: Slider | null, next: Slider | null ) {
+    constructor(prev: Slider | null, id: string) {
+        this.prev = prev
+        this.id = id
         this.position = (!!(prev?.position) ? prev.position + 1 : 1)
         this.value = this.position * 10
-        this.prev = prev
-        this.next = next
+        this.next = null
         this.body = document.createElement("div")
+        this.body.addEventListener("click", () => console.log("Event Working"))
         this.styles = ``
+        //Append it to slider container
     }
 
     moveValue() {
-        
+
     }
 }
+
+var mainSliderContainer: SliderContainer = new SliderContainer([])
+
+var sliderPropsArr: sliderInstanceProps[] = [
+    { prev: null, id: "" }
+]
+
+
