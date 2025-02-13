4882465c06812412ef7a7843db24b6d598b52783
Ryan <ryancfreas@gmail.com>
2025-02-13 15:43:08 -0600

    Adding type interfaces for styles

---
+++ src/Components/Typography/types.ts
@@ -0,0 +1,21 @@
+export interface Styles {
+  color: string
+  lineHeight: number
+  letterSpacing: number
+  fontFamily: string
+}
+
+export interface Font {
+  value: string
+  displayName: string
+}
+
+export interface Scale {
+  value: string
+  label: string
+}
+
+export interface Label {
+  htmlFor: string
+  text: string
+}
